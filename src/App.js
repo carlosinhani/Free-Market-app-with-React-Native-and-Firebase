91f6fd6b968614fd2e65d329fdc15097fd869551
Carlos.Inhani <carlos.inhani@gmail.com>
2020-11-25 10:19:06 -0300

    add_Navigation

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Navigation from "./Navigation";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Carlos Hello</Text>
-      <StatusBar style="auto" />
-    </View>
+     <Navigation /> 
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
