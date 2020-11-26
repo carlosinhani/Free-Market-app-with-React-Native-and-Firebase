0a1ce795daf726ddb984a301d0ca2615a1da7348
Carlos.Inhani <carlos.inhani@gmail.com>
2020-11-26 10:29:13 -0300

    index_navigation

--- src/view/Index/index.js
+++ src/view/Index/index.js
@@ -1,14 +1,63 @@
 import React from 'react';
-import { View, Image, Alert } from 'react-native';
+import { View, Image, Alert, Text } from 'react-native';
 import { createDrawerNavigator, DrawerContentScrollView, DrawerItem } from "@react-navigation/drawer";
+import { FontAwesome } from '@expo/vector-icons';
 import styles from "./styles";
 import Navigation from "./Navigation";
 
 const Drawer = createDrawerNavigator();
 
+const HeaderDrawer = () =>{
+  return(
+     <View>
+         <View>
+            <Text>Olá</Text>
+         </View>
+         <View>
+            <Text>Mercado Pago</Text>
+         </View>
+     </View>
+  )
+}
+
+const DrawerContent = ({ navigation }) => {
+    const listMenuDrawer = [
+      {
+          id: 1,
+          name: "Home",
+          action: "Home",
+          icon: <FontAwesome name="home" size={24} color="#000" />
+      },
+      {
+        id: 2,
+        name: "Procurar",
+        action: "List",
+        icon: <FontAwesome name="search-plus" size={24} color="#000" />
+    },
+  ]
+
+  return (
+     <View>
+        <HeaderDrawer />
+        <View>
+           <DrawerContentScrollView>
+                  {listMenuDrawer.map((menu)=>(
+                      <DrawerItem
+                      label={menu.name}
+                      key={menu.id}
+                      icon={()=>menu.icon}
+                      onPress={()=>navigation.navigate(menu.action)}
+                      />
+                  ))}
+           </DrawerContentScrollView>
+        </View>
+     </View>
+  )
+}
+
 export default function Index(){
   return (
-      <Drawer.Navigator>
+      <Drawer.Navigator drawerContent={DrawerContent}>
            <Drawer.Screen
               name="Navigation"
               component={Navigation}
@@ -16,4 +65,4 @@ export default function Index(){
       </Drawer.Navigator>
   );
 }
-
+ 
